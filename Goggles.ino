f51d01393a9c4f3317a2d409cb4a05f0bd4e0d13
pikesley <github@orgraphone.org>
2017-01-12 12:14:11 +0000

    Abstractions ahoy

--- Goggles.ino
+++ Goggles.ino
@@ -1,16 +1,13 @@
-// Low power NeoPixel goggles example.  Makes a nice blinky display
-// with just a few LEDs on at any time.
-
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 16
 #define BUTTON 9
+#define DELAY 35
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(32, PIN);
 
 uint8_t  mode   = 0, // Current animation effect
          offset = 0; // Position of spinny eyes
-uint32_t color  = 0xFF0000; // Start red
 uint32_t prevTime;
 
 void setup() {
@@ -25,28 +22,35 @@ void loop() {
 
   switch(mode) {
 
-   case 0:
-    for(i=0; i<32; i++) {
-      uint32_t c = 0;
-      if(((offset + i) & 7) < 2) c = color; // 4 pixels on...
-      pixels.setPixelColor(   i, c); // First eye
-     // pixels.setPixelColor(31-i, c); // Second eye (flipped)
-    }
-    pixels.show();
-    offset++;
-    delay(50);
+  case 0:
+    wheel(0xFF0000);
+    break;
+  case 1:
+    wheel(0x00FF00);
+    break;
+  case 2:
+    wheel(0x0000FF);
     break;
   }
 
   t = millis();
-  if((t - prevTime) > 2000) {      // Every 8 seconds...
-    mode++;                        // Next mode
-    if(mode > 0) {                 // End of modes?
-      mode = 0;                    // Start modes over
-      color >>= 8;                 // Next color R->G->B
-      if(!color) color = 0xFF0000; // Reset to red
+  if((t - prevTime) > DELAY * 16) {
+    mode++;
+    if(mode > 2) {
+      mode = 0;
     }
     for(i=0; i<32; i++) pixels.setPixelColor(i, 0);
     prevTime = t;
   }
+}
+
+void wheel(uint32_t colour) {
+  for(int i=0; i<32; i++) {
+    uint32_t c = 0;
+    if(((offset + i) & 7) < 2) c = colour;
+    pixels.setPixelColor(   i, c);
+  }
+  pixels.show();
+  offset++;
+  delay(DELAY);
 }
