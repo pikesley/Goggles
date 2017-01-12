39bd5dfae566d819cdb76ba9e0ab8f35c19d1f73
pikesley <github@orgraphone.org>
2017-01-12 16:26:22 +0000

    Juggle works

--- Goggles.ino
+++ Goggles.ino
@@ -1,54 +1,121 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 16
-#define BUTTON 9
+#define BUTTON 7
 #define DELAY 35
+#define PIXELS 16
+#define RIGHT_NOSE 14
+#define LEFT_NOSE 4
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(32, PIN);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXELS * 2, PIN);
 
-uint8_t  mode   = 0, // Current animation effect
+uint8_t  mode   = 3, // Current animation effect
          offset = 0; // Position of spinny eyes
-uint32_t prevTime;
+volatile byte changeMode = false;
 
 void setup() {
+  pinMode(BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), pin_ISR, FALLING);
   pixels.begin();
   pixels.setBrightness(85); // 1/3 brightness
-  prevTime = millis();
 }
 
 void loop() {
-  uint8_t  i;
-  uint32_t t;
-
-  switch(mode) {
-
-  case 0:
-    wheel(0xFF0000);
-    break;
-  case 1:
-    wheel(0x00FF00);
-    break;
-  case 2:
-    wheel(0x0000FF);
-    break;
-  }
-
-  t = millis();
-  if((t - prevTime) > DELAY * 16) {
+  if (changeMode) {
     mode++;
-    if(mode > 2) {
+    changeMode = false;
+    if (mode > 5) {
       mode = 0;
     }
-    for(i=0; i<32; i++) pixels.setPixelColor(i, 0);
-    prevTime = t;
+  }
+
+  switch(mode) {
+  case 0:
+    rollers(0xFF0000);
+    break;
+  case 1:
+    blankAll();
+    juggle(0xFFFF00);
+    break;
+  case 2:
+    rollers(0x00FF00);
+    break;
+  case 3:
+    blankAll();
+    juggle(0x00FFFF);
+    break;
+  case 4:
+    rollers(0x0000FF);
+    break;
+  case 5:
+    blankAll();
+    juggle(0xFF00FF);
+    break;
+
+
   }
 }
 
-void wheel(uint32_t colour) {
-  for(int i=0; i<32; i++) {
+void pin_ISR() {
+  changeMode = true;
+}
+
+void juggle(uint32_t colour) {
+  for(int i = 0; i < 1; i++) {
+    rollLeft(colour);
+    rollRight(colour);
+  }
+  for(int i = 0; i < 2; i++) {
+    rollLeft(colour);
+  }
+  for(int i = 0; i < 1; i++) {
+    rollRight(colour);
+    rollLeft(colour);
+  }
+  for(int i = 0; i < 2; i++) {
+    rollRight(colour);
+  }
+}
+
+void rollRight(uint32_t colour) {
+  for(int i = RIGHT_NOSE; i < PIXELS + RIGHT_NOSE; i++) {
+    lightOne(i % PIXELS, 0, colour);
+    delay(DELAY);
+  }
+
+  blankAll();
+}
+
+void rollLeft(uint32_t colour) {
+  for(int i = LEFT_NOSE + PIXELS; i > LEFT_NOSE; i--) {
+    lightOne(i % PIXELS, PIXELS, colour);
+    delay(DELAY);
+  }
+
+  blankAll();
+}
+
+void lightOne(int index, int offset, uint32_t colour) {
+  for(int i = offset; i < PIXELS + offset; i++) {
+    uint32_t c = 0;
+    if(i == index + offset) c = colour;
+    pixels.setPixelColor(i, c);
+  }
+  pixels.show();
+}
+
+void blankAll() {
+  for(int i = 0; i < PIXELS * 2; i++) {
+    pixels.setPixelColor(i, 0);
+  }
+  pixels.show();
+}
+
+void rollers(uint32_t colour) {
+  for(int i = 0; i < PIXELS * 2; i++) {
     uint32_t c = 0;
     if(((offset + i) & 7) < 2) c = colour;
-    pixels.setPixelColor(   i, c);
+    pixels.setPixelColor(i, c);
   }
   pixels.show();
   offset++;
